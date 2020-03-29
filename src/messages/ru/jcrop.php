146fbc864bf6b63eeb6a72210577fc2af4f80a13
beckson <becksonq@gmail.com>
2020-03-29 12:59:00 +0300

    create ru message file

---
+++ src/messages/ru/jcrop.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'Перетащите изображение' => 'Drag Photo',
+    'Выберите изображение' => 'Select Photo',
+    'или' => 'Or',
+    'Обрезать' => 'Crop Photo',
+    'Выбрать другое изображение' => 'Select Another Photo',
+    'File Size Error' => 'The file is too big. Its size cannot exceed {size} MB.',
+    'File Extension Error' => 'Only files with these extensions are allowed: {formats}.',
+    'Missing Attribute' => 'Attibute "{attribute}" must be specified',
+    'Only POST Request' => 'Allowed only POST-request',
+    'Can Not Upload File' => 'can not upload file',
+];
