b89566a215b5f039006c8b75f1648be34261e7e7
beckson <becksonq@gmail.com>
2020-03-29 13:19:56 +0300

    update ru message file

--- src/messages/ru/jcrop.php
+++ src/messages/ru/jcrop.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'Перетащите изображение' => 'Drag Photo',
-    'Выберите изображение' => 'Select Photo',
-    'или' => 'Or',
-    'Обрезать' => 'Crop Photo',
-    'Выбрать другое изображение' => 'Select Another Photo',
-    'File Size Error' => 'The file is too big. Its size cannot exceed {size} MB.',
-    'File Extension Error' => 'Only files with these extensions are allowed: {formats}.',
-    'Missing Attribute' => 'Attibute "{attribute}" must be specified',
-    'Only POST Request' => 'Allowed only POST-request',
-    'Can Not Upload File' => 'can not upload file',
+    'Drag Photo'=> 'Перетащите изображение' ,
+    'Select Photo' => 'Выберите изображение',
+    'Or' => 'или',
+    'Crop Photo' => 'Обрезать',
+    'Select Another Photo' => 'Выбрать другое изображение',
+    'File Size Error' => 'Файл слишком большой. Размер не должен превышать {size} MB.',
+    'File Extension Error' => 'Разрешены только файлы с расширениями: {formats}.',
+    'Missing Attribute' => 'Атрибут "{attribute}" не установлен',
+    'Only POST Request' => 'Разрешен только POST-request',
+    'Can Not Upload File' => 'Невозможно загрузить файл',
 ];
